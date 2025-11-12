a756b873c7974fd1c729cfe999fe552676d426a7
Surya Sharma <suryasharma73077@gmail.com>
2025-11-12 15:49:09 +0530

    Add files via upload

--- DoublyCircular.cpp
+++ DoublyCircular.cpp
@@ -24,6 +24,7 @@ class DoublyCircular{
 				head = tail =  newNode;
 				tail->next = head;
 				head->prev = head;
+				return;
 			}else{
 				newNode ->next = head;
 				newNode->prev = tail;
@@ -31,6 +32,75 @@ class DoublyCircular{
 				head->prev = newNode;
 				head = newNode;
 			}
+		}
+		void InsertBack(int val){
+			Node * newNode = new Node(val);
+			if(head == NULL){
+				head = tail =  newNode;
+				tail->next = head;
+				head->prev = head;
+				return;
+			}else{
+				tail->next = newNode;
+				newNode->prev = tail;
+				tail = newNode;
+				tail->next = head;
+				head->prev = tail;
+			}
+		}
+		void RemoveFront(){
+			if(head == NULL){
+				cout<<"List is Empty";
+				return;
+			}else if(head == tail){
+				delete head;
+				head = tail = NULL;
+				return;
+			}
+			Node * temp = head;
+			head = head->next;
+			tail->next = head;
+			head->prev = tail;
+			delete temp;
+		}
+		void RemoveLast(){
+			if(head == NULL){
+				cout<<"List is Empty";
+				return;
+			}else if(head == tail){
+				delete head;
+				head = tail =NULL;
+				return;
+			}
+			Node * temp =head;
+			while(temp->next!=tail){
+				temp = temp->next;
+			}
+			tail = temp;
+			temp = temp->next;
+			tail->next = head;
+			head->prev = tail;
+			delete temp;
+		}
+		void Search(int val){
+			if(head == NULL){
+				cout<<"List is Empty";
+				return;
+			}
+			Node * temp = head;
+			while(temp!=tail){
+				if(temp->data == val){
+					cout<<"True";
+					return;
+				}
+				temp = temp->next;
+			}
+			if(temp->data == val){
+					cout<<"True";
+					return;
+			}
+			cout<<"false";	
+			
 		}
 		void Display(){
 			if(head == NULL){
@@ -43,7 +113,22 @@ class DoublyCircular{
 				temp = temp->next;
 			}
 			while(temp!=head);
-			cout<<endl;
+			cout<<"End"<<endl;
+		}
+		
+		void DisplayReverse(){
+			if(head == NULL){
+				cout<<"List is Empty";
+				return;
+			}
+			Node * temp = tail;
+			while(temp!= head){
+				cout<<temp->data<<" <-> ";
+				temp = temp->prev;
+			}
+			cout<<temp->data<<" <-> ";
+			cout<<"End"<<endl;
+			
 		}
 };
 int main(){
@@ -52,5 +137,20 @@ int main(){
 	ds.InsertFront(13);
 	ds.InsertFront(14);
 	ds.InsertFront(15);
+	ds.InsertBack(43);
+	ds.InsertBack(7);
+	ds.RemoveFront();
+	ds.RemoveFront();
+//	ds.RemoveFront();
+//	ds.RemoveLast();
+//	ds.RemoveLast();
+//	ds.RemoveLast();
+//	ds.RemoveFront();
+	cout<<"\n";
+	cout<<"Display..."<<endl;
 	ds.Display();
+	cout<<"\n";
+	cout<<"Reverse..."<<endl;
+	ds.DisplayReverse();
+	ds.Search(23);
 }
